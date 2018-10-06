93bd1cdb433d5920b7b63deddbe3491bf55fcf3c
Mathijs <m.a.vandenworm@gmail.com>
2018-10-06 15:32:55 +0000

    Add the channel implementation

---
+++ include/channel.hpp
@@ -0,0 +1,89 @@
+#ifndef CHANNEL_INCREMENT_CHANNEL
+#define CHANNEL_INCREMENT_CHANNEL
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+
+template<typename ValueType>
+struct Channel
+{
+   struct Receiver
+   {
+      Receiver(Channel<ValueType>* incomming, ValueType& recipient)
+         : incomming(incomming)
+         , recipient(recipient)
+      {}
+      Receiver(Receiver& other)
+         : Receiver(other.incomming, other.recipient)
+      {
+         other.incomming = nullptr;
+      }
+
+      operator bool()
+      {
+         Channel<ValueType>* receiving = incomming;
+         incomming = nullptr;
+         return receiving && receiving->try_receive(recipient);
+      }
+
+      ~Receiver()
+      {
+         if (incomming)
+            incomming->blocking_receive(recipient);
+      }
+
+   private:
+      Channel<ValueType>* incomming;
+      ValueType& recipient;
+   };
+
+public:
+   bool empty() {
+      std::lock_guard<std::mutex> guard(key);
+      return messages.empty();
+   }
+
+   void send(const ValueType& message)
+   {
+      std::lock_guard<std::mutex> guard(key);
+      messages.push(message);
+      received.notify_all();
+   }
+
+   bool try_receive(ValueType& recipient)
+   {
+      bool succeded = false;
+      std::unique_lock<std::mutex> guard(key, std::try_to_lock);
+      if(guard.owns_lock()){
+         if (!messages.empty()) {
+            recipient = messages.front();
+            messages.pop();
+            succeded = true;
+         }
+      }
+      return succeded;
+   }
+
+   void blocking_receive(ValueType& recipient)
+   {
+      std::lock_guard<std::mutex> guard(key);
+      received.wait(key, [this]() { return !messages.empty(); });
+      recipient = messages.front();
+      messages.pop();
+   }
+
+   void operator<<(const ValueType& message) { send(message); }
+   Receiver operator>>(ValueType& recipient)
+   {
+      return Receiver(this, recipient);
+   }
+
+private:
+   std::mutex key;
+   std::condition_variable_any received;
+   std::queue<ValueType> messages;
+};
+
+#endif
